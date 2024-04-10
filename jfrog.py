e22bc9cfa6934c59c5f6ff41928187b8228df0aa
biswaraj <biswaraj1999@gmail.com>
2024-04-10 22:26:39 +0530

    Added jfrog.py file

---
+++ jfrog.py
@@ -0,0 +1,32 @@
+import requests
+
+
+def upload_jar_to_artifactory(jfrog_repo_url, username, password, jar_file_path):
+    try:
+        # Constructing the upload URL
+        upload_url = f"{jfrog_repo_url}/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
+
+        # authentication
+        auth = (username, password)
+
+        # Read the JAR file
+        with open(jar_file_path, "rb") as jar_file:
+            # Upload the JAR file to Artifactory
+            response = requests.put(upload_url, auth=auth, data=jar_file)
+
+        if response.status_code == 201:
+            return f"Successfully pushed {jar_file_path} to JFrog Artifactory."
+        else:
+            return f"Failed to push {jar_file_path} to Artifactory. Status code: {response.status_code}\n{response.text}"
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+
+
+# main:
+jfrog_repo_url = "http://100.26.165.59:8082/artifactory/jar-push-using-python"
+username = "admin"
+password = "Pass@123"
+jar_file_path = "/var/lib/jenkins/workspace/assignmentPipeline/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
+
+result = upload_jar_to_artifactory(jfrog_repo_url, username, password, jar_file_path)
+print(result)
